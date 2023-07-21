c8f07194e5c58aebcc2207c06d4d1b69a40ab316
Cryfari <75016427+Cryfari@users.noreply.github.com>
2023-07-22 06:54:46 +0700

    postgress database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -24,12 +24,12 @@ class NotesHandler {
    * @param {any} h
    * @return {response} response
    */
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const {title = 'untitled', body, tags} = request.payload;
 
-      const noteId = this._service.addNote({title, body, tags});
+      const noteId = await this._service.addNote({title, body, tags});
 
       const response = h.response({
         status: 'success',
@@ -64,8 +64,8 @@ class NotesHandler {
   /**
    * @return {response} response
    */
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
       status: 'success',
       data: {
@@ -79,10 +79,10 @@ class NotesHandler {
    * @param {any} h
    * @return {response} response
    */
-  getNoteByIdHandler(request, h) {
+  async getNoteByIdHandler(request, h) {
     try {
       const {id} = request.params;
-      const note = this._service.getNoteById(id);
+      const note = await this._service.getNoteById(id);
       return {
         status: 'success',
         data: {
@@ -115,12 +115,12 @@ class NotesHandler {
    * @param {any} h
    * @return {response} response
    */
-  putNoteByIdHandler(request, h) {
+  async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const {id} = request.params;
 
-      this._service.editNoteById(id, request.payload);
+      await this._service.editNoteById(id, request.payload);
       return {
         status: 'success',
         message: 'Catatan berhasil diperbarui',
@@ -151,11 +151,11 @@ class NotesHandler {
    * @param {any} h
    * @return {response} response
    */
-  deleteNoteByIdHandler(request, h) {
+  async deleteNoteByIdHandler(request, h) {
     try {
       const {id} = request.params;
 
-      this._service.deleteNoteById(id);
+      await this._service.deleteNoteById(id);
 
       return {
         status: 'success',
