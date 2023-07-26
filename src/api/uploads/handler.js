6006cf2dd7e1dcdbaefeb4575ad978aa35300bee
Cryfari <75016427+Cryfari@users.noreply.github.com>
2023-07-27 06:37:20 +0700

    local storage implemented

---
+++ src/api/uploads/handler.js
@@ -0,0 +1,57 @@
+const ClientError = require('../../exceptions/ClientError');
+/**
+ * uploads handler
+ */
+class UploadsHandler {
+  /**
+   * @param {service} service
+   * @param {validator} validator
+   */
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUploadImageHandler = this.postUploadImageHandler.bind(this);
+  }
+
+  /**
+   * @param {request} request
+   * @param {hapi} h
+   */
+  async postUploadImageHandler(request, h) {
+    try {
+      const {data} = request.payload;
+      this._validator.validateImageHeaders(data.hapi.headers);
+
+      const filename = await this._service.writeFile(data, data.hapi);
+
+      const response = h.response({
+        status: 'success',
+        data: {
+          fileLocation: `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = UploadsHandler;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 /* eslint-disable max-len */
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
+const path = require('path');
 
 // notes
 const notes = require('./api/notes');
@@ -28,6 +30,11 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+// uploads
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
+
 require('dotenv').config();
 
 const init = async () => {
@@ -35,6 +42,7 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -49,6 +57,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
@@ -105,6 +116,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
